225f743011b09e4d016abfe205d0913bdc2a1637
Martin Cuchta <cuchta@bart.sk>
2017-02-11 00:40:15 +0100

    Report generator

--- api/routes/wcs.js
+++ api/routes/wcs.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router();
+const ObjectId = require('mongoose').Types.ObjectId;
 
 const Wc = require('../schemas/wc');
+const Report = require('../schemas/report');
 
 router.get('/', (req, res) => {
     const params = req.query;
@@ -27,6 +29,31 @@ router.get('/', (req, res) => {
         });
     }
 });
+
+
+/**
+ * Report generator
+ */
+// const data = require('./data');
+// const moment = require('moment');
+// router.get('/gen', (req, res) => {
+//     for (let item of data) {
+//         if (item.status === 'occupied' || item.status === 'vacant') {
+//             let report = new Report();
+//             report.date = moment(item.date);
+//             report.status = item.status === 'occupied' ? true : false;
+//             report.wcId = new ObjectId('589e3bf575d52d722a3d2c08');
+//             report.save();
+
+//             let report2 = new Report();
+//             report2.date = moment(item.date);
+//             report2.status = item.status === 'occupied' ? false : true;
+//             report2.wcId = new ObjectId('589e3cda75d52d722a3d2cda');
+//             report2.save();
+//         }
+//     }
+//     res.json({});
+// });
 
 router.get('/:token', (req, res) => {
     Wc.findById(req.params.token, (err, item) => {
